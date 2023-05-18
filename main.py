f818aa6bf87c3c6c498dd2019e139dee80d0086c
MohammedYasserr <bmbmyasser@gmail.com>
2023-05-18 18:40:58 +0300

    Definig the schema of the body request

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI 
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI() 
+
+
+# constructing a class that defines the schema structure
+
+class Post(BaseModel):
+    title: str 
+    content : str
+    published : bool = True
+    rating : Optional[int] = None
 
 
 @app.get("/")
@@ -15,7 +26,7 @@ def get_posts():
 
 
 @app.post("/createposts")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {'New Post': f"title: {payload['title']} content: {payload['content']}"}
+def create_posts(new_post : Post):
+    print(new_post.rating)
+    return {"Data" : "New Post"}
 
